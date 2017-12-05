8bbbe559dfe817e83d3ef0418850ddc5b673a497
Kyle Tenzeldam and Natalia Telpukhova <kyle@>
2017-12-05 09:46:55 -0800

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,20 @@
 $(document).ready(function(){
-  $(".numbers").submit(function(event) {
+  $(".puzzle").submit(function(event) {
     event.preventDefault();
-    var firstnumber = $("input#firstnumber").val();
-    var secondnumber = $("input#secondnumber").val();
 
-    if ((secondnumber < 0) || (0 > firstnumber)) {
-      $("#output").append("negative numbers");
-    } else if (secondnumber < firstnumber){
-      $("#output").append("count by number is larger than the count to numbers");
-    } else if (/[^0-9]/g) {
-      $("#output").append("Not a number");
-    } else {
-      for (var total = 1; total <= firstnumber/secondnumber; total += 1) {
-        number = total * secondnumber;
-        $("#output").append(number + " ");
+    var sentence = $("input#sentence").val();
+    sentence = sentence.toLowerCase();
+    var sentenceArray = sentence.split('');
+
+    for (var index = 0; index < sentenceArray.length; index += 1) {
+      if ((sentenceArray[index] === 'a') || (sentenceArray[index] === 'o') || (sentenceArray[index] === 'e') || (sentenceArray[index] === 'i') || (sentenceArray[index] === 'u') || (sentenceArray[index] === 'y')) {
+        sentenceArray[index] = '-';
       }
     }
 
-    // for (var number = secondnumber; number <= firstnumber; number += secondnumber) {
-    //   number = total * secondnumber;
-    //   $("#output").append(number + " ");
-    // }
+    sentence = sentenceArray.join('')
+
+    $("#output").text(sentence);
 
     $(".output").show();
     $(".numbers").hide();
